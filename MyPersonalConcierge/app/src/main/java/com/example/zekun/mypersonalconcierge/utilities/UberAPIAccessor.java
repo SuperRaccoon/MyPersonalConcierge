63927031dca098b7f3468c3777de2917d44039c5
unknown <mikebolt@gmail.com>
2015-10-10 12:03:48 -0700

    Implement queryAllDestinations.

--- MyPersonalConcierge/app/src/main/java/com/example/zekun/mypersonalconcierge/utilities/UberAPIAccessor.java
+++ MyPersonalConcierge/app/src/main/java/com/example/zekun/mypersonalconcierge/utilities/UberAPIAccessor.java
@@ -4,6 +4,8 @@ import com.example.zekun.mypersonalconcierge.models.Coordinates;
 import com.example.zekun.mypersonalconcierge.models.UberPriceEstimate;
 import com.example.zekun.mypersonalconcierge.models.UberPriceEstimateQuery;
 
+import com.example.zekun.mypersonalconcierge.utilities.DestinationDatabaseAccessor;
+
 import java.util.List;
 import java.util.Set;
 
@@ -15,7 +17,21 @@ public class UberAPIAccessor {
 
     public static List<UberPriceEstimate> queryAllDestinations(Coordinates startCoordinates,
             Set<String> acceptableUberProductNames) {
-        // TODO: create a bunch of queries and return all their results
-        return null;
+        // TODO: this will be really inefficient if there are tons of destinations
+
+        List<UberPriceEstimate> allPriceEstimates = new ArrayList<UberPriceEstimate>();
+
+        List<Destination> allDestinations = DestinationDatabaseAccessor.getAllDestinations();
+
+        for (Destination possibleDestination : allDestinations) {
+            // Create a query for the current possible destination
+            UberPriceEstimateQuery query = new UberPriceEstimateQuery(startCoordinates,
+                    acceptableUberProductNames, possibleDestination);
+
+            List<UberPriceEstimate> estimatesForCurrentDestination = estimatePrice(query);
+            allPriceEstimates.addAll(estimatesForCurrentDestination);
+        }
+
+        return allPriceEstimates;
     }
 }
