fabf6b8373970811eba484f633ae5d631c20b1be
unknown <mikebolt@gmail.com>
2015-10-10 14:32:06 -0700

    Finish implementing UberAPIAccessor.

--- MyPersonalConcierge/app/src/main/java/com/example/zekun/mypersonalconcierge/utilities/UberAPIAccessor.java
+++ MyPersonalConcierge/app/src/main/java/com/example/zekun/mypersonalconcierge/utilities/UberAPIAccessor.java
@@ -17,6 +17,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.StringBuffer;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
@@ -25,66 +26,101 @@ import java.util.List;
 import java.util.Set;
 
 public class UberAPIAccessor {
-    String server_id = "$SECRET";
+    public static final String SERVER_ID = "$SECRET";
+
+    public static final String API_CALL_PROTOCOL = "https";
+    public static final String API_CALL_DOMAIN = "sandbox-api.uber.com";
+    public static final String API_CALL_VERSION = "v1";
+    public static final String API_CALL_ENDPOINT = "estimates/price";
+
+    static {
+        public static final Set<String> DEFAULT_PRODUCTS =
+                new HashSet<String>();
+
+        DEFAULT_PRODUCTS.add("UberX"); // Just use this one for now.
+    }
+
+    /**
+     * This method blocks. queryAllDestinations does not.
+     */
     public static List<UberPriceEstimate> estimatePrice(UberPriceEstimateQuery query) {
-        // TODO: implement this
+
+        StringBuffer apiCall = new StringBuffer();
+        apiCall.append(API_CALL_PROTOCOL);
+        apiCall.append("://");
+        apiCall.append(API_CALL_DOMAIN);
+        apiCall.append("/");
+        apiCall.append(API_CALL_VERSION);
+        apiCall.append("/");
+        apiCall.append(API_CALL_ENDPOINT);
+        apiCall.append("?");
+
+        // Add the coordinate parameters
+        apiCall.append("start_latitude=");
+        apiCall.append(Double.toString(query.getStartCoordinates().getLatitude()));
+
+        apiCall.append("&");
+
+        apiCall.append("start_longitude=");
+        apiCall.append(Double.toString(query.getStartCoordinates().getLongitude()));
+
+        apiCall.append("&");
+
+        apiCall.append("end_latitude=");
+        apiCall.append(Double.toString(query.getDestinationCoordinates().getLatitude()));
+
+        apiCall.append("&");
+
+        apiCall.append("end_longitude=");
+        apiCall.append(Double.toString(query.getDestinationCoordinates().getLongitude()));
+
+        String parsedString = "";
+
+        try {
+            URL url = new URL(apiCall.toString());
+            URLConnection conn = url.openConnection();
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.setRequestProperty("Authorization", "Token " + SERVER_ID);
+            httpConn.connect();
+
+            InputStream is = httpConn.getInputStream();
+            parsedString = convertinputStreamToString(is);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        // TODO: parse and stuff
+
         return null;
     }
-    private class UBERLookup extends AsyncTask<Destination, Integer, String> {
-        //coords = {end lat, end long}
-        protected String doInBackground(Destination... dest) {
-            for(Destination desstination : dest){
 
+    public static List<UberPriceEstimate> getEstimatesFromJSON(String json,
+                                                               UberPriceEstimateQuery query) {
+        List<UberPriceEstimate> estimates = new List<UberPriceEstimate>();
 
+        JSONObject rootObject = new JSONOjbect(json);
 
+        JSONArray prices = rootObject.getJSONArray("prices");
 
+        for (int i = 0; i < prices.length(); ++i) {
+            JSONObject price = prices.getJSONObject(i);
+
+            String productType = price.getString("display_name"); // TODO: use IDs instead
+
+            // Ignore prices with productTypes not in the acceptable set.
+            if (query.getAcceptableUberProductNames().contains(productType)) {
+                // Make an UberPriceEstimate to encapsulate the data
+                UberPriceEstimate uberPriceEstimate =
+                        new UberPriceEstimate(query, price.getDouble("high_estimate"), productType);
+                estimates.add(uberPriceEstimate);
             }
-            String parsedString = "";
-
-            try {
-                String api_call = "https://sandbox-api.uber.com/v1/estimates/price?start_latitude=" + start_latitude + "&start_longitude=" + start_longitude + "&end_latitude=" + coords[0] + "&end_longitude=" + coords[1];
-                URL url = new URL(api_call);
-                URLConnection conn = url.openConnection();
-                HttpURLConnection httpConn = (HttpURLConnection) conn;
-                httpConn.setAllowUserInteraction(false);
-                httpConn.setInstanceFollowRedirects(true);
-                httpConn.setRequestMethod("GET");
-                httpConn.setRequestProperty("Authorization", "Token " + server_id);
-                httpConn.connect();
-
-                InputStream is = httpConn.getInputStream();
-                parsedString = convertinputStreamToString(is);
-                return parsedString;
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return "failed";
         }
 
-
-        protected void onPostExecute(String result) {
-
-            try {
-                JSONObject mainObject = new JSONObject(result);
-                JSONArray prices = mainObject.getJSONArray("prices");
-                String estimates = "";
-                JSONObject offer;
-                //uncomment to get prices for every car option :D
-                //for(int i=0; i<prices.length();i++){
-
-                offer = prices.getJSONObject(0);//i);
-                estimates += offer.getString("localized_display_name") + ": " + offer.getString("estimate") + "\n" + offer.getString("distance") + " miles" + "\n";
-                //}
-
-                //THIS IS WHERE WE HANDLE THE POST-PROCESS DATA
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-
-
-        }
+        return estimates;
     }
 
     public static String convertinputStreamToString(InputStream ists)
@@ -107,26 +143,56 @@ public class UberAPIAccessor {
             return "";
         }
     }
+
     public static List<UberPriceEstimate> queryAllDestinations(Coordinates startCoordinates,
-            Set<String> acceptableUberProductNames) {
+            Set<String> acceptableUberProductNames, List<UberPriceEstimate> results,
+             final Runnable onFinishCallback) {
         // TODO: this will be really inefficient if there are tons of destinations
 
-        List<UberPriceEstimate> allPriceEstimates = new ArrayList<UberPriceEstimate>();
+        // Define the AsyncTask that performs this function asynchronously.
+        // It will be able to access some of this function's values through pseudo-closure,
+        // even after this function returns.
+        class QueryAllDestinationsTask extends
+                AsyncTask<UberPriceEstimateQuery, Integer, List<UberPriceEstimate>> {
+            public Object doInBackground(UberPriceEstimateQuery... queries) {
+                List<UberPriceEstimate> result = new ArrayList<UberPriceEstimate>();
 
+                int progress = 0;
+                publishProgress(progress);
+                for (UberPriceEstimateQuery query : queries) {
+                    List<UberPriceEstimate> priceEstimates = estimatePrice(query);
+                    ++progress;
+                    publishProgress(progress);
+                    result.addAll(priceEstimates);
+                }
+
+                return result;
+            }
+
+            public void onPostExecute(Object result) {
+                // Hey, we're finished!
+                results.addAll(result);
+                onFinishCallback.run();
+            }
+        }
+
+        // Generate all the queries
         DestinationDatabaseAccessor destinationDatabaseAccessor =
                 new EmptyDestinationDatabaseAccessor();
 
         List<Destination> allDestinations = destinationDatabaseAccessor.getAllDestinations();
+        List<UberPriceEstimateQuery> allQueries = new ArrayList<UberPriceEstimateQuery>();
 
         for (Destination possibleDestination : allDestinations) {
             // Create a query for the current possible destination
+
             UberPriceEstimateQuery query = new UberPriceEstimateQuery(startCoordinates,
                     acceptableUberProductNames, possibleDestination);
-
-            List<UberPriceEstimate> estimatesForCurrentDestination = estimatePrice(query);
-            allPriceEstimates.addAll(estimatesForCurrentDestination);
+            allQueries.add(query);
         }
 
-        return allPriceEstimates;
+        allQueries.toArray(new UberPriceEstimateQuery[]);
+        // Start the AsyncTask, which will not block this thread, then return.
+        new QueryAllDestinationsTask().execute(allQueries);
     }
 }
