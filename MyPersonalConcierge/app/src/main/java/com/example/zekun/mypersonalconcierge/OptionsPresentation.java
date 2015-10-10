c0889fab9821e7aed24c4175b550bd4a985d892a
SuperRaccoon <zekunwang@berkeley.edu>
2015-10-10 13:12:22 -0700

    Backup

--- MyPersonalConcierge/app/src/main/java/com/example/zekun/mypersonalconcierge/OptionsPresentation.java
+++ MyPersonalConcierge/app/src/main/java/com/example/zekun/mypersonalconcierge/OptionsPresentation.java
@@ -1,10 +1,22 @@
 package com.example.zekun.mypersonalconcierge;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 
 public class OptionsPresentation extends ActionBarActivity {
@@ -19,6 +31,15 @@ public class OptionsPresentation extends ActionBarActivity {
         Intent myGivenInfo = getIntent();
 
         myBudgetInt = Integer.parseInt(getIntent().getStringExtra("inputBudget"));
+
+
+        myUberClass myUberObj1 = new myUberClass();
+        myUberObj1.setDescCostName("Big C is a cool place to be",180, "Big C");
+
+        myUberClass myUberObj2 = new myUberClass();
+        myUberObj2.setDescCostName("This place is not furd",20, "UC Berkeley");
+
+
 
 
     }
@@ -43,5 +64,92 @@ public class OptionsPresentation extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void initializeList(Context myContext){
+
+        ViewGroup parent = (ViewGroup) findViewById(R.id.myScrollLinear);
+        parent.removeAllViews();
+
+
+
+        //First we grab the file
+
+        File myFile = new File(myFileLocation);
+        ArrayList<myMusicClass> myList = new ArrayList<>();
+
+        if(myFile.exists() && !myFile.isDirectory()) {
+
+            try {
+                FileInputStream streamIn = new FileInputStream(myFileLocation);
+                ObjectInputStream objectinputstream = new ObjectInputStream(streamIn);
+                ArrayList<myMusicClass> retrievedList = (ArrayList) objectinputstream.readObject();
+                objectinputstream.close();
+                streamIn.close();
+
+                myList = retrievedList;
+                mySuperList = myList;
+
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+        /*Now that we have the ArrayList stored in the file, we iterate through it to grab each
+        individual music entry to display in the ScrollView
+         */
+
+        for (int e=0;e<myList.size();e++) {
+
+            myMusicClass myMusicObj = myList.get(e);
+
+            String myArtist = myMusicObj.artist;
+            String mySong = myMusicObj.song;
+            String myAlbum = myMusicObj.album;
+            int myId = myMusicObj.id;
+
+            /*Now we take care of setting it up visually
+
+             */
+
+            parent = (ViewGroup) findViewById(R.id.myScrollLinear);
+
+            View view = LayoutInflater.from(myContext).inflate(R.layout.basic_song_unit, null);
+
+            final TextView myTextview = (TextView) view.findViewById(R.id.displayWindow);
+            myTextview.setText("Artist: " + myArtist + "\nSong: " + mySong + "\nAlbum: " + myAlbum);
+
+            final TextView myIdHolder = (TextView) view.findViewById(R.id.idHolder);
+            myIdHolder.setText(Integer.toString(myId));
+
+            final Button deleteButton = (Button) view.findViewById(R.id.button);
+            deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mySuperList != null) {
+                        for (int r = 0; r < mySuperList.size(); r++) {
+                            myMusicClass myCurrObj = (mySuperList.get(r));
+                            if (myCurrObj.id == Integer.parseInt((String) myIdHolder.getText())) {
+                                removeAtIndex(r);
+                            }
+                        }
+                        ((ViewManager) v.getParent()).removeView(myTextview);
+                        ((ViewManager) v.getParent()).removeView(deleteButton);
+
+
+                    }
+                    refresh();
+                }
+
+            });
+
+
+            parent.addView(view);
+        }
     }
 }
---
+++ MyPersonalConcierge/app/src/main/java/com/example/zekun/mypersonalconcierge/myUberClass.java
@@ -0,0 +1,25 @@
+package com.example.zekun.mypersonalconcierge;
+
+/**
+ * Created by Zekun on 10/10/2015.
+ */
+public class myUberClass {
+
+    String name;
+
+    String description;
+
+    int Cost;
+
+
+
+
+    public void setDescCostName(String inputDesc, int inputCost, String inputName){
+
+        this.description = inputDesc;
+        this.Cost = inputCost;
+        this.name = inputName;
+    }
+
+
+}
